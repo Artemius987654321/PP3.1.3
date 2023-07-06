416e164ba68de95b7e76d446466bb8b0c7ed29fb
Artemius987654321] <artyom.lazarev.1999@bk.ru]>
2023-07-06 13:52:28 +0700

    PP3.1.3
    PP3.1.3

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,81 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class AdminController {
+    private final UserService userService;
+    private final RoleService roleService;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AdminController(UserService userService, RoleService roleService, @Lazy PasswordEncoder passwordEncoder) {
+
+        this.userService = userService;
+        this.roleService = roleService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "admin";
+    }
+
+    @GetMapping("/admin/users")
+    public String findAll(Model model) {
+        List<User> users = userService.getAllUsers();
+        model.addAttribute("users", users);
+        return "users";
+    }
+
+    @GetMapping("/admin/create")
+    public String createUserForm(User user, Model model) {
+        List<Role> allRoles = roleService.getAllRoles();
+        model.addAttribute("roleSelect", allRoles);
+        return "create";
+    }
+
+    @PostMapping("/admin/create")
+    public String createUser(User user) {
+        userService.saveUser(user);
+        return "redirect:/admin/users";
+    }
+
+    @GetMapping("/admin/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
+        userService.removeUserById(id);
+        return "redirect:/admin/users";
+    }
+
+    @GetMapping("/admin/edit/{id}")
+    public String updateUserForm(@PathVariable("id") Long id, Model model) {
+        User user = userService.getUserById(id);
+        user.setPassword("");
+        model.addAttribute("user", user);
+        List<Role> allRoles = roleService.getAllRoles();
+        model.addAttribute("roleSelect", allRoles);
+        return "edit";
+    }
+
+    @PostMapping("/admin/edit")
+    public String updateUser(User user) {
+        userService.saveUser(user);
+        return "redirect:/admin/users";
+    }
+}
